2cd039671d9658eda6cae1939108a8137246f96f
marceloosg <marcelosg@gmail.com>
2018-12-07 06:50:09 -0200

    refactored fit function; TODO fix lda fitting

--- fitPca.R
+++ fitPca.R
@@ -1,14 +1,20 @@
 
-fitLda=function(training){
+fit=function(training, meth="lda"){
   ctrl <- trainControl(method = "repeatedcv",
-                       repeats = 5, number = 10,
+                       repeats = 20, number = 5,
                        verbose = TRUE,
-                       classProbs = TRUE)
-
+                       classProbs = TRUE,
+                       )
   results <- train(group ~ .,
                            data = training,
-                           method = "lda",
+                           method = meth,
                            trControl = ctrl,
-                           metric = "Accuracy", # not needed it is so by default
                            importance=TRUE)  
+}
+
+
+validate=function(){
+  predicted=predict(lda,newdata = testdata)
+  tested=testdata$group
+  table(predicted,tested)
 }
